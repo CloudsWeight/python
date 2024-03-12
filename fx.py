3c189b752870923592a7551c29602e0cbfa0e6c0
CloudsWeight <CloudsWeight@users.noreply.github.com>
2024-03-12 00:28:27 -0700

    Update fx.py

--- fx.py
+++ fx.py
@@ -29,17 +29,27 @@ import requests
 import json 
 from accts import accts
 from secret import SECRET 
-#import argparse
+import argparse
+import matplotlib.pyplot as plt
+import mplfinance as mpf
 
 class OandaApp():
-	def __init__(self, token='', account=''):
+	def __init__(self, token=None, account=None):
 		self.BASE_URL ='https://api-fxtrade.oanda.com'
+		if account is not None:
+			self.account = {
+					'URL':f'{self.BASE_URL}/v3/accounts', # Accounts Endpoint
+					'acctId': account,
+					'current':f'{account}'
+					}
 		self.account = {
 					'URL':f'{self.BASE_URL}/v3/accounts', # Accounts Endpoint
 					'acctId': accts,
 					'current':f'{accts[0]}'
 						}
-		self.token = SECRET
+		#if token is None:
+			#self.token = SECRET
+		self.token = token
 		self.HEADER = { # Headers and Auth
 					'Authorization': 'Bearer {}'.format(SECRET),
 				} 
@@ -57,7 +67,7 @@ class OandaApp():
 					}
 		self.count = {
 						'URL':'count=',
-						'current':'3' # [default=500, maximum=5000] dont use with "FROM and TO"
+						'current':'36' # [default=500, maximum=5000] dont use with "FROM and TO"
 						}
 		self.candles = {'URL':'/candles?',
 						'granularity':{
@@ -77,27 +87,32 @@ class OandaApp():
 			 f"{self.candles['URL']}"
 			 f"{self.count['URL']}{self.count['current']}"
 			 f"{self.candles['granularity']['current']}"
-			 				)
-# Expected Built URL: https://api-fxtrade.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5
-						
-		self.query_url = f"{self.instruments['URL']}{self.instruments['current']}"
-
+			 				) # As Above So Below
+	# Example:	https://api-fxtrade.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5
+	######################## methods, man
 	def query_accounts(self, url=None, header=None):
 		if header is None:
 			header = self.HEADER
 		if url is None:
-			url = f"{self.account['URL']}{self.account['current']}"
-		
+			url = f"{self.account['URL']}"
 		r = requests.get(url=url, headers=header)
 		print(f"{r.status_code}, /n {r.content}")
 
-	def query_rate(self, url=None, header=None):
+	def query_account(self, url=None, header=None):
+		if header is None:
+			header = self.HEADER
+		if url is None:
+			url = f"{self.account['URL']}/{self.account['current']}"
+			print(url)
+		r = requests.get(url=url, headers=header)
+		print(f"{r.status_code}")
 
+	def query_rate(self, url=None, header=None):
 		if header is None:
 			header = self.HEADER
 		if url is None:
 			url = self.current_url
-		print(self.current_url)
+		print(f"\nURL: {self.current_url}")
 		r = requests.get(url=url, headers=header)
 		#print(r.status_code)
 		#print(r.content)
@@ -109,12 +124,44 @@ class OandaApp():
 		else:
 			json_string = bin_data.decode('utf-8')
 			json_data = json.loads(json_string)
-			print(json_data)
+			#print(json.dumps(json_data, indent=2))
+		return json_data
 
-#                                                   
+	def update_current_url(self):
+		self.current_url = (
+					 f"{self.instruments['URL']}{self.instruments['current']}"
+					 f"{self.candles['URL']}"
+					 f"{self.count['URL']}{self.count['current']}"
+					 f"{self.candles['granularity']['current']}"
+					 )
 
+	def set_rate(self, rate=None):
+		if rate is None:
+			print("No rate set no change made")
+		rate = rate.lower()
+		self.instruments['current'] = self.instruments[rate]
+		print(f"new rate {self.instruments['current']}\n ")     
+		self.update_current_url()
+
+class Candlesticks():
+	def __init__(self):
+		self.fig, self.ax = plt.subplots()
+		self.ohlc_data = []
+
+	def fetch_data(self, data=None):
+		if data is None:
+			print("Failed to fetch_data")
+		self.ohlc_data =[]
+		print(data['candles'])
+		#for candle in data['candles']:
+		#	if 'mid' in candle:
+		#		self.ohlc_data.append([[candle['time'], candle['mid']['o'], candle['mid']['h'],candle['mid']['l'],candle['mid']['c']]])
+		return data
+			 	
+		
+
+################## Enter the dragon
 if __name__ == '__main__':
 	app = OandaApp()
 	raw_data = app.query_rate()
-	app.deserialize(raw_data)
-
+	app.query_account()
